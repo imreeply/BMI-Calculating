1c14c250ce70754bbed8b4574faf435f9bd788f0
imreeply <rezasaputra2014@gmail.com>
2022-10-08 00:42:34 +0700

    Update JS

--- script.js
+++ script.js
@@ -3,21 +3,21 @@ let tinggi, berat, keterangan, bmi;
 function hitung() {
   tinggi = parseFloat(document.getElementById("tinggi").value);
   tinggi /= 100;
-  berat = parseFloat(document.getElementById("berat").value); // jika bilangan didalamnya mengandung desimal maka akan ditampilkan semua nilainya.
+  berat = parseFloat(document.getElementById("berat").value);
 
   bmi = berat / (tinggi * tinggi);
   if (bmi < 18.5) {
     keterangan = "Underweight &#128524;";
   } else if ((bmi >= 18.5) & (bmi <= 24.9)) {
     keterangan = "Normal &#128515";
-  } else if (bmi >= 25 && bmi <= 29.9) {
+  } else if ((bmi >= 25) & (bmi <= 29.9)) {
     keterangan = "Overwight &#128551";
   } else if (bmi > 30) {
     keterangan = "Obecity &#128552";
-  } // Else If untuk mencari kondisi yang diinginkan
+  }
 
   document.getElementById("bmi").innerHTML = "Hasil  : " + bmi.toFixed(2);
-  document.getElementById("keterangan").innerHTML = "Status : " + keterangan; // Nampilin Hasil dari Else if
+  document.getElementById("keterangan").innerHTML = "Hasil : " + keterangan;
 
   formInput.reset();
 }
